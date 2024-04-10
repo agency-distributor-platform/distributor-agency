e11a8dde8fcfbf99a1ff8ea56771d5a2b25cdac9
Roshan Basu <roshanbasu7@gmail.com>
2024-04-10 23:31:54 +0530

    Add employer details in user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,28 @@
 class UserController < AuthenticationController
 
   def current_user_details
-    derived_user_details = derive_user_details(user.as_json)
+    derived_user_details = derive_user_details(user)
     render json: derived_user_details, status: 200
   end
 
   def user_details
     user_id = convert_uuid_to_id(params[:user_id])
-    derived_user_details = derive_user_details(User.find_by(id: user_id).as_json)
+    derived_user_details = derive_user_details(User.find_by(id: user_id))
     render json: derived_user_details, status: 200
   end
 
   private
 
-  def derive_user_details(user_details)
+  def derive_user_details(user_record)
+    user_details = user_record.as_json
     user_details["id"] = convert_id_to_uuid(user_details["id"])
     user_details["employer_id"] = convert_id_to_uuid(user_details["employer_id"])
     user_details.delete("password")
+
+    user_details["user_type_details"] = user_record.employer.as_json
+    user_details["user_type_details"]["id"] = convert_id_to_uuid(user_details["user_type_details"]["id"])
+    user_details["user_type_details"]["agency_id"] = convert_id_to_uuid(user_details["user_type_details"]["agency_id"]) if user_details["user_type_details"]["agency_id"].present?
+
     user_details
   end
 
