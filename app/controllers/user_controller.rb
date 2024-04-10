4969f00c44529a8c077c371b5045c5094658bf65
Roshan Basu <roshanbasu7@gmail.com>
2024-04-10 18:30:35 +0530

    Removing Password

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,7 @@ class UserController < AuthenticationController
   def current_user_details
     user_details = user.as_json
     user_details["employer_id"] = convert_id_to_uuid(user_details["employer_id"])
+    user_details.delete("password")
     render json: user_details, status: 200
   end
 
@@ -10,6 +11,7 @@ class UserController < AuthenticationController
     user_id = convert_uuid_to_id(params[:user_id])
     user_details = User.find_by(id: user_id).as_json
     user_details["employer_id"] = convert_id_to_uuid(user_details["employer_id"])
+    user_details.delete("password")
     render json: user_details, status: 200
   end
 
