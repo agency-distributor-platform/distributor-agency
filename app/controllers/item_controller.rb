85e757caf5c05bb02b894b598d644706afd4b947
Roshan Basu <roshanbasu7@gmail.com>
2024-05-12 09:25:40 +0530

    Debugging

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -42,6 +42,8 @@ class ItemController < AuthenticationController
                     else
                       BusinessLogic::AgencyObj.new(employer.as_json.deep_symbolize_keys)
                     end
+    Rails.logger.error(employer_obj)
+    Rails.logger.error("----------------")
     render json: {earnings: employer_obj.get_earnings}
   end
 
