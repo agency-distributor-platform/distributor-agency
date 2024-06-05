48d761f343bcb1b2efb806fdb02eda0bcc189ffe
Pravesh Nautiyal <pravesh.nautiyal@pramata.com>
2024-06-06 00:29:10 +0530

    Added constants

---
+++ config/initializers/constants.rb
@@ -0,0 +1,7 @@
+module Constants
+  #add value here
+  VEHICLE_DETAILS_UPLOAD_HEADERS = []
+  NOT_A_VALID_EXTENSION_MESSAGE = "Not a valid extension"
+  FILE_HAS_MORE_THAN_ONE_SHEET_MESSAGE = "The file has more than one sheet"
+  NO_RECORDS_FOUND_MESSAGE = "No records found in the sheet"
+end
