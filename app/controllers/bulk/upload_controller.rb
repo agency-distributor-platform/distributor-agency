cc00bea10b536d2f970c62acca6eb975d82bd323
Pravesh Nautiyal <pravesh.nautiyal@pramata.com>
2024-06-06 22:36:57 +0530

    Updated controller to download file

--- app/controllers/bulk/upload_controller.rb
+++ app/controllers/bulk/upload_controller.rb
@@ -1,17 +1,11 @@
 module Bulk 
   class UploadController < ApplicationController
     def vehicle_details_template
-      file = Tempfile.new("vehicle_details_upload")
-      records = []
-      records << Constants::VEHICLE_DETAILS_UPLOAD_HEADERS
+      file = 'vehicle_details_template.xlsx'
+      records = [Constants::VEHICLE_DETAILS_UPLOAD_HEADERS]
+      p records
       Utils::FileParserFactory.get_parser('xlsx').write(file, records)
-      file.rewind
-      yield file if block_given?
-      #send_file(file)
-      send_file(file.path, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', disposition: 'attachment', filename: 'vehicle_details_template.xlsx')
-    ensure
-      file.close
-      file.unlink
+      send_file(file, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', disposition: 'attachment')
     end
   end 
 end 
