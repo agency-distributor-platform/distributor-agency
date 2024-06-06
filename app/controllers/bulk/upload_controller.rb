13f0f0cd06a54af56188f732f719ae3aabe374ed
Pravesh Nautiyal <pravesh.nautiyal@pramata.com>
2024-06-06 22:57:14 +0530

    Updated headers for vehicle details bulk upload template

--- app/controllers/bulk/upload_controller.rb
+++ app/controllers/bulk/upload_controller.rb
@@ -3,7 +3,6 @@ module Bulk
     def vehicle_details_template
       file = 'vehicle_details_template.xlsx'
       records = [Constants::VEHICLE_DETAILS_UPLOAD_HEADERS]
-      p records
       Utils::FileParserFactory.get_parser('xlsx').write(file, records)
       send_file(file, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', disposition: 'attachment')
     end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 module Constants
   #add value here
-  VEHICLE_DETAILS_UPLOAD_HEADERS = ["test"]
+  VEHICLE_DETAILS_UPLOAD_HEADERS = ["ID Number", "Fin company Name", "Loan / Agrement Number", "Model", "REG NO", "ENGINE NO",	"CHASIS NO",	"Year of Manufacturing",	"Stock entery date", "Dealer / Location", "Buy Price",	"Sale Price",	"Commission", "Due",	"Remark", "Profit / loss"]
   NOT_A_VALID_EXTENSION_MESSAGE = "Not a valid extension"
   FILE_HAS_MORE_THAN_ONE_SHEET_MESSAGE = "The file has more than one sheet"
   NO_RECORDS_FOUND_MESSAGE = "No records found in the sheet"
