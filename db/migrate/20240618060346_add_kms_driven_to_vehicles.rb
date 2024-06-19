2e5db46c05850218667db2b4f0e80178571ca1e8
Pravesh Nautiyal <pravesh.nautiyal@pramata.com>
2024-06-19 12:15:10 +0530

    code review changes

--- db/migrate/20240618060346_add_kms_driven_to_vehicles.rb
+++ db/migrate/20240618060346_add_kms_driven_to_vehicles.rb
@@ -1,5 +1,8 @@
+require_relative "./migration_utils.rb"
 class AddKmsDrivenToVehicles < ActiveRecord::Migration[7.0]
+  include MigrationUtils
+  TABLE_NAME = :vehicles
   def change
-    add_column :vehicles, :kms_driven, :bigint
+    add_with_verification_column(TABLE_NAME, :kms_driven, :bigint)
   end
 end
--- db/migrate/20240618075016_add_details_to_inquiries.rb
+++ db/migrate/20240618075016_add_details_to_inquiries.rb
@@ -1,7 +1,10 @@
+require_relative "./migration_utils.rb"
 class AddDetailsToInquiries < ActiveRecord::Migration[7.0]
+  include MigrationUtils
+  TABLE_NAME = :inquiries
   def change
-    add_column :inquiries, :name, :string
-    add_column :inquiries, :email, :string
-    add_column :inquiries, :phone, :bigint
+    add_with_verification_column(TABLE_NAME, :name, :string)
+    add_with_verification_column(TABLE_NAME, :email, :string)
+    add_with_verification_column(TABLE_NAME, :phone, :bigint)
   end
 end
