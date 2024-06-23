bf04920c99f17dad8261d094404f27f021ebcbe9
Pravesh Nautiyal <pravesh.nautiyal@pramata.com>
2024-06-23 22:59:29 +0530

    Added migration to trigger on add_ons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_22_090621) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_23_172108) do
   create_table "add_ons", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.float "amount"
     t.string "biller_name"
